82fb43049c0e8ed11b7e8a62c09e2f2609078dd3
bravo <andflores2006@gmail.com>
2026-03-23 21:46:39 -0600

    Backup y cambio en frm de asistencia

---
+++ ListaAsistencia/backups/backup_asistencia_20260323_181049.sql
@@ -0,0 +1,89 @@
+-- MySQL dump 10.13  Distrib 9.6.0, for Linux (x86_64)
+--
+-- Host: localhost    Database: asistencia
+-- ------------------------------------------------------
+-- Server version	9.6.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET @MYSQLDUMP_TEMP_LOG_BIN = @@SESSION.SQL_LOG_BIN;
+SET @@SESSION.SQL_LOG_BIN= 0;
+
+--
+-- GTID state at the beginning of the backup 
+--
+
+SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ '75321dc4-1e59-11f1-bfc0-8e9f28cdbe29:1-28';
+
+--
+-- Table structure for table `Alumnos`
+--
+
+DROP TABLE IF EXISTS `Alumnos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Alumnos` (
+  `nControl` varchar(8) NOT NULL,
+  `nombre` varchar(60) NOT NULL,
+  `apPaterno` varchar(30) NOT NULL,
+  `apMaterno` varchar(30) NOT NULL,
+  PRIMARY KEY (`nControl`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Alumnos`
+--
+
+LOCK TABLES `Alumnos` WRITE;
+/*!40000 ALTER TABLE `Alumnos` DISABLE KEYS */;
+INSERT INTO `Alumnos` VALUES ('22130067','Ronaldinho','De Souza','Huerta'),('24130796','Carlos Andrés','Flores','Rosales');
+/*!40000 ALTER TABLE `Alumnos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Asistencia`
+--
+
+DROP TABLE IF EXISTS `Asistencia`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Asistencia` (
+  `fecha` varchar(45) NOT NULL,
+  `nControl` varchar(45) NOT NULL,
+  `vino` varchar(45) NOT NULL DEFAULT 'NO',
+  PRIMARY KEY (`fecha`,`nControl`),
+  KEY `nControl_idx` (`nControl`),
+  CONSTRAINT `nControl` FOREIGN KEY (`nControl`) REFERENCES `Alumnos` (`nControl`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Asistencia`
+--
+
+LOCK TABLES `Asistencia` WRITE;
+/*!40000 ALTER TABLE `Asistencia` DISABLE KEYS */;
+INSERT INTO `Asistencia` VALUES ('23/03/2026','22130067','NO'),('23/03/2026','24130796','NO');
+/*!40000 ALTER TABLE `Asistencia` ENABLE KEYS */;
+UNLOCK TABLES;
+SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-03-24  0:10:49
